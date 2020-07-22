c1f007d3e3bc51cfbd452806eb922978b936d419
moscuet <mostifizur.rahman@integrify.iogit config --global user.email mostifizur.rahman@integrify.io>
2020-07-22 21:53:25 +0300

    Roman Numeral Converter

---
+++ Palindrome Checker.js
@@ -0,0 +1,28 @@
+console.log(' Palindrome JS connected')
+function convertToRoman(num) {
+    const romanObj = {
+        M: 1000,
+        CM: 900,
+        D: 500,
+        CD: 400,
+        C: 100,
+        XC: 90,
+        L: 50,
+        XL: 40,
+        X: 10,
+        IX: 9,
+        V: 5,
+        IV: 4,
+        I: 1
+      };
+      let romanStr=''
+      for (let key of Object.keys(romanObj)) {
+        let n = Math.floor(num / romanObj[key]);
+        num = num- n * romanObj[key];
+        romanStr += key.repeat(n);
+      }
+      return romanStr;
+    }
+    
+  console.log( convertToRoman(3642))
+    
